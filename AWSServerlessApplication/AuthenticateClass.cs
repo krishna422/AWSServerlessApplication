eaca0052a9a0ed757884907a7130a69aec640efe
Sai Krishna Eswarabhatla <Saikrishna.Eswarabhatla@valuemomentum.com>
2021-08-06 16:48:54 +0530

    Add project files.

---
+++ AWSServerlessApplication/AuthenticateClass.cs
@@ -0,0 +1,14 @@
+﻿namespace AWSServerlessApplication
+{
+    public class AuthenticateClass
+    {
+
+        public string hostName;
+        public string username;
+        public string password;
+        public string authorizationToken;
+        public string expiresTimestamp;
+
+    }
+
+}
---
+++ AWSServerlessApplication/Controllers/SocotraController.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWSServerlessApplication.Controllers
+{
+    [Route("api/socotra")]
+    public class SocotraController : ControllerBase
+    {
+        const string baseURL = "https://api.sandbox.socotra.com/";
+
+        [HttpGet]
+        [Route("getvalues")]
+        public string GetValues()
+        {
+            return "{\"fieldValues\":{\"external_value_here\":[\"Hello from aws function app\"]}}";
+        }
+    }
+}
